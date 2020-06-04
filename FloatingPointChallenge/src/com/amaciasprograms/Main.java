2bbf6d0d87e5bd71d1ec7906af62438ce278e031
Art Macias <35348623+amaciasjr@users.noreply.github.com>
2020-06-04 16:21:55 -0600

    Remove KilometersToMilesConverter from FloatingPointChallenge

--- FloatingPointChallenge/src/com/amaciasprograms/Main.java
+++ FloatingPointChallenge/src/com/amaciasprograms/Main.java
@@ -3,7 +3,10 @@ package com.amaciasprograms;
 public class Main {
 
     private static final double KILOGRAM_PER_POUND = 0.45359237;
-    private static final double MILES_PER_KILOMETER = 0.621371;
+
+    private static double convertPoundsToKilograms(int pound) {
+        return pound / KILOGRAM_PER_POUND;
+    }
 
     public static void main(String[] args) {
         int pound = 10;
@@ -11,18 +14,5 @@ public class Main {
 
         System.out.println(pound + " pounds is equal to " + kilogramValue + " kilograms.");
 
-
-        double milesConversion = convertKilometersToMiles();
-
     }
-
-    private static double convertPoundsToKilograms(int pound) {
-        return pound / KILOGRAM_PER_POUND;
-    }
-
-    private static double convertKilometersToMiles(double kilometers) {
-        return kilometers * MILES_PER_KILOMETER;
-    }
-
-
 }
