a2af8c29ea0a6dffe52ab051ac977a8ddd43f22f
Art Macias <35348623+amaciasjr@users.noreply.github.com>
2020-06-04 16:23:58 -0600

    Fix mistakes in FloatingPointChallenge
    Previously converted pounds to kilograms by dividing
    by Kilogram per pound ratio, instead of mulitplying
    the pounds by the ratio.

--- FloatingPointChallenge/src/com/amaciasprograms/Main.java
+++ FloatingPointChallenge/src/com/amaciasprograms/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     private static final double KILOGRAM_PER_POUND = 0.45359237;
 
-    private static double convertPoundsToKilograms(int pound) {
-        return pound / KILOGRAM_PER_POUND;
+    private static double convertPoundsToKilograms(double pound) {
+        return pound * KILOGRAM_PER_POUND;
     }
 
     public static void main(String[] args) {
-        int pound = 10;
+        double pound = 100d;
         double kilogramValue = convertPoundsToKilograms(pound);
 
         System.out.println(pound + " pounds is equal to " + kilogramValue + " kilograms.");
